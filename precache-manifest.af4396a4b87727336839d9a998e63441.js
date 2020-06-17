2afe40cecdeddcb11d33a6b843b5cb4644ad8881
Ezequielpereyraa <ezequielpereyra@hotmail.com>
2020-06-17 12:01:40 -0300

    Updates

---
+++ precache-manifest.af4396a4b87727336839d9a998e63441.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6969c358ef2795fdf210ff744fba61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "534fc06a217a3253e6ed",
+    "url": "/static/css/main.bc0c500c.chunk.css"
+  },
+  {
+    "revision": "9bed86b62202f5d0125f",
+    "url": "/static/js/2.921f53fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.921f53fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "534fc06a217a3253e6ed",
+    "url": "/static/js/main.a3816aab.chunk.js"
+  },
+  {
+    "revision": "867a340ab582ce62f7a0",
+    "url": "/static/js/runtime-main.71a434a2.js"
+  }
+]);
